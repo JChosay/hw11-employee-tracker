e206f1d9e2e7c16155011ab34788d65aca2c4ff5
Joshua Chosay <jjchosay@gmail.com>
2021-07-18 15:37:43 -0500

    More progress.

--- index.js
+++ index.js
@@ -2,29 +2,36 @@ const { Console } = require('console');
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
+const { Sequelize } = require('sequelize');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Yonjuuni$%646042',
-    database: 'cms_db',
-  });
-  
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'jj1234',
+//     database: 'cms_db',
+//   });
 
+cons
+
+//! Sweet banner, right? See: https://manytools.org/hacker-tools/ascii-banner/
+
+console.log('----------------------------------------------------------------------');
+console.log('----------------------------------------------------------------------');
 console.log('███████╗███╗   ███╗██████╗ ██╗      ██████╗ ██╗   ██╗███████╗███████╗');
 console.log('██╔════╝████╗ ████║██╔══██╗██║     ██╔═══██╗╚██╗ ██╔╝██╔════╝██╔════╝');
 console.log('█████╗  ██╔████╔██║██████╔╝██║     ██║   ██║ ╚████╔╝ █████╗  █████╗  ');
 console.log('██╔══╝  ██║╚██╔╝██║██╔═══╝ ██║     ██║   ██║  ╚██╔╝  ██╔══╝  ██╔══╝  ');
 console.log('███████╗██║ ╚═╝ ██║██║     ███████╗╚██████╔╝   ██║   ███████╗███████╗');
 console.log('╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝');
-console.log('');
-console.log('████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗ ');
-console.log('╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗');
-console.log('   ██║   ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝');
-console.log('   ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗');
-console.log('   ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║');
-console.log('   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝');
+console.log('     ████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗ ');
+console.log('     ╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗');
+console.log('        ██║   ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝');
+console.log('        ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗');
+console.log('        ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║');
+console.log('        ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝');
+console.log('----------------------------------------------------------------------');
+console.log('----------------------------------------------------------------------');
 console.log("Welcome to YerTEAM CMS, a product of FRUOsoft!")
 
 //TODO: this code functions; use it to auto-populate list choices from the database.
@@ -75,57 +82,57 @@ function mainPrompt(){
         }else if (answers.prompt === "Update Employee Manager"){
             updateManager();
         }else if (answers.prompt === "Quit"){
+            console.log('----------------------------------------------------------------------');
+            console.log('     ██████╗  ██████╗  ██████╗ ██████╗ ██████╗ ██╗   ██╗███████╗██╗');
+            console.log('    ██╔════╝ ██╔═══██╗██╔═══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝██╔════╝██║');
+            console.log('    ██║  ███╗██║   ██║██║   ██║██║  ██║██████╔╝ ╚████╔╝ █████╗  ██║');
+            console.log('    ██║   ██║██║   ██║██║   ██║██║  ██║██╔══██╗  ╚██╔╝  ██╔══╝  ╚═╝');
+            console.log('    ╚██████╔╝╚██████╔╝╚██████╔╝██████╔╝██████╔╝   ██║   ███████╗██╗');
+            console.log('     ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝');
+            console.log('----------------------------------------------------------------------');
             console.log("Thanks for using YerTEAM CMS.");
+            connection.end();
         }
     })
 }
 
 mainPrompt();
 
-// function AllEmployees(){
-//     console.log("AllEmployees selected");
-//      const mainPrompt = () => {
-    
-//         connection.query('SELECT * FROM position', (err, results) => {
-//          if (err) throw err;
-
-//          inquirer.prompt([
-//              {
-//                  message: 'What action would you like to take?',
-//                 type: 'list',
-//                  name: 'prompt',
-//                  choices () {
-//                      const actionArray = [];
-//                      results.forEach(({title}) => {
-//                          actionArray.push(title);
-//                      });
-//                      return actionArray;   
-//                  },
-//              }
-//          ])
-//          .then((answer) => {
-//              let chosenAction;
-//          })
-//      })
-//  }
+//! This works; but, it doesn't pull in the right fields to satisfy the reqs.
+// const queryAllEmployees = () => {
+//     connection.query('SELECT * FROM employee', (err, res) => {
+//         if (err) throw err;
+//         res.forEach(({ id, first_name, last_name, role_id, manager_id}) => { 
+//         });
+        
+//         console.log('----------------------------------------------');
+//         console.log("Viewing All Employees:")
+//         console.log('----------------------------------------------');
+//         console.table(res);
+//         console.log('----------------------------------------------');
+//         mainPrompt();
+//     });
+// };
 
 const queryAllEmployees = () => {
-    connection.query('SELECT * FROM employee', (err, res) => {
+    let query = 'SELECT employee.id, employee.first_name, employee.last_name, position.title, position.salary, employee.manager_id';
+    query += 'FROM (employee INNER JOIN position ON employee.role_id = position.id)';
+
+    connection.query(query, (err, res) => {
         if (err) throw err;
-        res.forEach(({ id, first_name, last_name, role_id, manager_id}) => { 
-        });
-        
+        res.ForEach(({id, first_name, last_name, title, salary, manager_id}) => {
+        })
+
         console.log('----------------------------------------------');
         console.log("Viewing All Employees:")
         console.log('----------------------------------------------');
         console.table(res);
         console.log('----------------------------------------------');
         mainPrompt();
-    });
+    })
 };
 
-
-                                                                     
+                                            
 
 // function employeesByDept(){
 //     console.log("EmployeesByDept selected");
